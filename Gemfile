2866014779994bf03337b2ec32b8b79d7d77e242
f1nwe <kaban.volodymyr@gmail.com>
2018-01-08 20:33:33 +0200

    added basic gems: rspec, slim, simple_form, faker, factory_bot and etc.

--- Gemfile
+++ Gemfile
@@ -9,24 +9,44 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'bootstrap-sass', '~> 3.3.6'
 gem 'coffee-rails', '~> 4.2'
+gem 'font-awesome-rails'
 gem 'jbuilder', '~> 2.5'
+gem 'jquery-rails'
+gem 'kaminari'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.7'
 gem 'rails', '~> 5.1.4'
 gem 'sass-rails', '~> 5.0'
+gem 'simple_form'
+gem 'slim-rails'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
+gem 'unobtrusive_flash', '>=3'
+
+# TODO: do not need in production (for seeding in prod)
+gem 'awesome_print'
+gem 'database_cleaner'
+gem 'factory_bot_rails'
+gem 'faker', git: 'git://github.com/stympy/faker.git', branch: 'master'
 
 group :development, :test do
+  # gem 'any_login'
   gem 'capybara', '~> 2.13'
   gem 'pry-rails'
+  gem 'rspec-rails'
   gem 'selenium-webdriver'
 end
 
 group :development do
+  gem 'annotate'
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'web-console', '>= 3.3.0'
+end
+
+group :test do
+  gem 'simplecov'
 end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+# Prevent database truncation if the environment is production
+
+if Rails.env.production?
+  abort('The Rails environment is running in production mode!')
+end
+
+require 'rspec/rails'
+
+# Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  # database cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  # include methods depending on file location
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+
+  # include factory bot methods
+  config.include FactoryBot::Syntax::Methods
+end
