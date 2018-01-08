604cfd149f1dfe381cc7f64f039926f0951ae9e2
f1nwe <kaban.volodymyr@gmail.com>
2018-01-08 20:33:33 +0200

    added basic gems: rspec, slim, simple_form, faker, factory_bot and etc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # TODO: move to the base controller of client app
+  after_action :prepare_unobtrusive_flash
 end
