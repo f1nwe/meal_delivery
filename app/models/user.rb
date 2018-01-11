c21c8ae6cae8a6317b0f0b1a621213b55dc15a60
f1nwe <kaban.volodymyr@gmail.com>
2018-01-11 21:56:01 +0200

    added role to user, fixed displaying errors on signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 # == Schema Information
 #
 # Table name: users
@@ -9,8 +8,27 @@
 #  name                :string           default(""), not null
 #  encrypted_password  :string           default(""), not null
 #  remember_created_at :datetime
+#  role                :integer          default("user")
 #
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable
+
+  ROLES = %i[user admin].freeze
+
+  enum role: ROLES
+
+  before_create :set_admin, if: :first_user?
+
+  validates :name, presence: true, length: { in: 3..15 }
+
+  private
+
+  def set_admin
+    self.role = :admin
+  end
+
+  def first_user?
+    self.class.count.zero?
+  end
 end
